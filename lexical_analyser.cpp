6952c0d0ee03ee7f7d197f1513661a5abf2ae236
Ramon Gazoni <ramongazonil@gmail.com>
2023-08-29 19:24:15 -0300

    Bug fixed

--- lexical_analyser.cpp
+++ lexical_analyser.cpp
@@ -18,16 +18,16 @@ int main (int argc, char *argv[]) {
     return 0;
 }
 
-void unnecessary_characters_dump (char c, std::ifstream &file) {
+void unnecessary_characters_dump (char c, std::ifstream &file) { 
     while ((c == '{' || c == ' ') && !file.eof() ) {
         if (c == '{') {
             while (c != '}' && !file.eof()) {
                 file.get(c);
             }
             file.get(c);
-            while(c == ' ' && !file.eof()) {
-                file.get(c);
-            }
+        }
+        while(c == ' ' && !file.eof()) {
+            file.get(c);
         }
     }
     if ( !file.eof() ) {
