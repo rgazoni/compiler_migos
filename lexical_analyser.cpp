2bdc0711b07620209a146681d0d2fd6ef88691d1
Ramon Gazoni <ramongazonil@gmail.com>
2023-08-31 10:32:13 -0300

    linked list insertion and handling special chars and reserved words

--- lexical_analyser.cpp
+++ lexical_analyser.cpp
@@ -5,10 +5,22 @@
 class Node {
 public:
   std::string lexem;
-  std::string simbol;
+  std::string symbol;
   Node *next;
 };
 Node *head = NULL;
+
+void insert_node(Node *node) {
+  if (head == NULL) {
+    head = node;
+  } else {
+    Node *temp = head;
+    while (temp->next != NULL) {
+      temp = temp->next;
+    }
+    temp->next = node;
+  }
+}
 
 void unnecessary_characters_dump(char c, std::ifstream &file);
 
@@ -21,7 +33,8 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   char c;
-  while (file.get(c)) {
+  file.get(c);
+  while (!file.eof()) {
     unnecessary_characters_dump(c, file);
   }
   file.close();
@@ -51,23 +64,59 @@ void handle_reserved_words_and_identifiers(char c, std::ifstream &file) {
   // Add the first character to the word string
   word += c;
   file.get(c);
-
+  // Form lexem
   while (isalpha(c) || isdigit(c) || c == '_' && !file.eof()) {
     word += c;
     file.get(c);
   }
+  Node *token = new Node();
+  token->lexem = word;
 
-  if (word == "if" || word == "else" || word == "while" || word == "do" ||
-      word == "break" || word == "continue" || word == "int" ||
-      word == "double" || word == "float" || word == "return" ||
-      word == "char" || word == "case" || word == "char" || word == "sizeof" ||
-      word == "long" || word == "short" || word == "typedef" ||
-      word == "switch" || word == "unsigned" || word == "void" ||
-      word == "static" || word == "struct" || word == "goto" ||
-      word == "auto" || word == "enum" || word == "extern" ||
-      word == "register" || word == "signed") {
-    std::cout << word << " is a reserved word" << std::endl;
+  if (word == "programa") {
+    token->symbol = "sprograma";
+  } else if (word == "se") {
+    token->symbol = "sse";
+  } else if (word == "entao") {
+    token->symbol = "sentao";
+  } else if (word == "senao") {
+    token->symbol = "ssenao";
+  } else if (word == "enquanto") {
+    token->symbol = "senquanto";
+  } else if (word == "faca") {
+    token->symbol = "sfaca";
+  } else if (word == "inicio") {
+    token->symbol = "sinicio";
+  } else if (word == "fim") {
+    token->symbol = "sfim";
+  } else if (word == "escreva") {
+    token->symbol = "sescreva";
+  } else if (word == "leia") {
+    token->symbol = "sleia";
+  } else if (word == "var") {
+    token->symbol = "svar";
+  } else if (word == "inteiro") {
+    token->symbol = "sinteiro";
+  } else if (word == "booleano") {
+    token->symbol = "sbooleano";
+  } else if (word == "verdadeiro") {
+    token->symbol = "sverdadeiro";
+  } else if (word == "falso") {
+    token->symbol = "sfalso";
+  } else if (word == "procedimento") {
+    token->symbol = "sprocedimento";
+  } else if (word == "funcao") {
+    token->symbol = "sfuncao";
+  } else if (word == "div") {
+    token->symbol = "sdiv";
+  } else if (word == "e") {
+    token->symbol = "se";
+  } else if (word == "ou") {
+    token->symbol = "sou";
+  } else if (word == "nao") {
+    token->symbol = "snao";
   } else {
-    std::cout << word << " is an identifier" << std::endl;
+    token->symbol = "sidentificador";
   }
+
+  insert_node(token);
 }
