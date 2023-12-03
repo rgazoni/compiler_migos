454193e77ea6582a5ff93716bfa187142d053c02
Ramon Gazoni <ramongazonil@gmail.com>
2023-12-03 20:36:03 -0300

    desse jeito o cod func media

---
+++ virtual_machine/build/CMakeFiles/VM.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ramongazoni/Documents/compiler/virtual_machine/src/main.cpp" "CMakeFiles/VM.dir/src/main.cpp.o" "gcc" "CMakeFiles/VM.dir/src/main.cpp.o.d"
+  "/Users/ramongazoni/Documents/compiler/virtual_machine/src/ui/application.cpp" "CMakeFiles/VM.dir/src/ui/application.cpp.o" "gcc" "CMakeFiles/VM.dir/src/ui/application.cpp.o.d"
+  "/Users/ramongazoni/Documents/compiler/virtual_machine/src/ui/fileexplorer.cpp" "CMakeFiles/VM.dir/src/ui/fileexplorer.cpp.o" "gcc" "CMakeFiles/VM.dir/src/ui/fileexplorer.cpp.o.d"
+  "/Users/ramongazoni/Documents/compiler/virtual_machine/src/ui/mainwindow.cpp" "CMakeFiles/VM.dir/src/ui/mainwindow.cpp.o" "gcc" "CMakeFiles/VM.dir/src/ui/mainwindow.cpp.o.d"
+  "/Users/ramongazoni/Documents/compiler/virtual_machine/src/virtual_machine/DVM.cpp" "CMakeFiles/VM.dir/src/virtual_machine/DVM.cpp.o" "gcc" "CMakeFiles/VM.dir/src/virtual_machine/DVM.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
